846380417236bbce8a54411ed0f7485e31dfb528
SURAJ BALARAM PATIL <78692972+SURAJPATIL6088@users.noreply.github.com>
2023-07-22 00:02:21 +0530

    initial setup

---
+++ web3/hardhat.config.js
@@ -0,0 +1,22 @@
+require("@matterlabs/hardhat-zksync-solc");
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: {
+    version: "0.8.17",
+    defaultNetwork:'goerli',
+    networks:{
+      hardhat: {},
+      goerli:{
+        url:'https://rpc.ankr.com/eth_goerli',
+        accounts: [`0x${process.env.PRIVATE_KEY}`]
+      }
+    },
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+};
