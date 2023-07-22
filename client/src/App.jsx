4bf38a05f6bb57ffe88fa031c99d4647883b9c85
SURAJ BALARAM PATIL <78692972+SURAJPATIL6088@users.noreply.github.com>
2023-07-22 19:49:55 +0530

    Navigation Functionality Added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,20 +5,25 @@ import { Route, Routes } from "react-router-dom";
 import { CampaignDetails, CreateCampaign, Profile, Home } from "./pages";
 
 // components
-import {Sidebar, Navbar} from './components';
+import { Sidebar, Navbar } from "./components";
 
 const App = () => {
   return (
     <div className="relative sm:p-8 p-4 bg-[#13131a] min-h-screen flex flex-row">
-      <div className="sm:flex hidden mr-10 relative"><Sidebar/></div>
-
-      <div className="flex-1 max-sm:w-full max-w-[1280px] mx-auto sm:pr-5">
-        <Navbar/>
+      <div className="sm:flex hidden mr-10 relative">
+        <Sidebar />
       </div>
 
-      <Routes>
-        <Route path="/" element={<Home/>}/>
-      </Routes>
+      <div className="flex-1 max-sm:w-full max-w-[1280px] mx-auto sm:pr-5">
+        <Navbar />
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/profile" element={<Profile/>}/>
+          <Route path="/create-campaign" element={<CreateCampaign/>}/>
+          <Route path="/campaign-details" element={<CampaignDetails/>}/>
+        </Routes>
+      </div>
     </div>
   );
 };
--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,2 +1,3 @@
 export {default as Sidebar} from './Sidebar';
 export {default as Navbar} from './Navbar';
+export {default as CustomButton} from './CustomButton';
