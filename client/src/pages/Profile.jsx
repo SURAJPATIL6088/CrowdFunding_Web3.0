ab896b7638ec7b9fe701bc289c8273c9b606a98a
SURAJ BALARAM PATIL <78692972+SURAJPATIL6088@users.noreply.github.com>
2023-07-28 16:39:15 +0530

    Profile Page Updated

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
 
-function Profile() {
-  return (
-    <div>Profile</div>
-  )
-}
+import DisplayCampaigns from "../components/DisplayCampaigns"
+import { useStateContext } from "../context";
+
+const Profile = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [campaigns, setCampaigns] = useState([]);
+
+  const { address, contract, getUserCampaigns } = useStateContext();
+  
+  const fetchCampaigns = async() => {
+    setIsLoading(true);
+    const data = await getUserCampaigns();
+    setCampaigns(data);
+    setIsLoading(false);
+  }
+
+  useEffect(() => {
+    if(contract) fetchCampaigns();
+  }, [address, contract]);
+
+  return(
+    <DisplayCampaigns
+        title="All Campaigns"
+        isLoading= {isLoading}
+        campaigns = {campaigns}
+    />
+  );
+};
 
 export default Profile;
